96c49519f98de99989fe3a88137257ca213b4167
apoorva <dograapoorva@outlook.com>
2025-08-12 20:41:03 -0400

    modified factorial

--- src/main/java/com/ankur/misc/Factorial.java
+++ src/main/java/com/ankur/misc/Factorial.java
@@ -1,5 +1,15 @@
 package com.ankur.misc;
 
 public class Factorial {
-    
+
+    public static void main(String[] args) {
+        System.out.println(factorial_Apoorva(4));
+    }
+    public static long factorial_Apoorva(int num){
+        int fact = 1;
+        for(int i=1; i<=num;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
 }
