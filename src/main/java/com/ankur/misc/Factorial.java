34408e7a68f12b5d917a010844ff8d85de63f3c3
ankurBtl <ankuryuman@yahoo.in>
2025-08-12 20:40:32 -0400

    updating factorial

--- src/main/java/com/ankur/misc/Factorial.java
+++ src/main/java/com/ankur/misc/Factorial.java
@@ -1,5 +1,13 @@
 package com.ankur.misc;
 
 public class Factorial {
-    
+    public static void main(String[] args) {
+        System.out.println(factorial_Ankur(5));
+    }
+    public static long factorial_Ankur(int num){
+        if(num==0){
+            return 1;
+        }
+        return num * factorial_Ankur(num-1);
+    }
 }
