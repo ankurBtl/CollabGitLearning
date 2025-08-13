34d60a85ca70a4061a1180aac58687b74575495a
apoorva <dograapoorva@outlook.com>
2025-08-12 20:24:00 -0400

    adding code for isEven to check odd even num

---
+++ src/main/java/com/ankur/misc/OddEven.java
@@ -0,0 +1,16 @@
+package com.ankur.misc;
+
+public class OddEven {
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+    }
+    public static boolean isEven(int num){
+
+        if(num%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
